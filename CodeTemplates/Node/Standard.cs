cedb30a35161951910363b30ebf8e51711258b8b
lightyears <lightyears1998@hotmail.com>
2023-12-30 19:53:19 +0800

    .NET 8 又快又强大

---
+++ CodeTemplates/Node/Standard.cs
@@ -0,0 +1,8 @@
+namespace PaperSudoku;
+
+public partial class _CLASS_ : _BASE_
+{
+    public override void _Ready()
+    {
+    }
+}
---
+++ Scenes/MainScene.cs
@@ -0,0 +1,10 @@
+namespace PaperSudoku;
+
+[SceneTree]
+public partial class MainScene : Control
+{
+    public override void _Ready()
+    {
+        GD.Print("OP.");
+    }
+}
