8113e6f79aaee31add2153aeb801ba63c5c8c707
lightyears <lightyears1998@hotmail.com>
2024-02-19 15:24:54 +0800

    Bring up unit tests

---
+++ Tests/PaperSudoku.Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+using PaperSudoku.Core;
+
+namespace PaperSudoku.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Sudoku sudoku = new();
+    }
+}
